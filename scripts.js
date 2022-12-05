d498f83bdd21affb28b8da6a4023ce8342b7976e
Nicholas <lynick14@gmail.com>
2022-12-05 15:14:19 -0500

    create function to play a round and to play a game of 5 rounds

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 // this is the code for our rock paper scissors game
-
-
 // declare a function that will get computer choice for rock paper scissor
 function getComputerChoice() {
     let max = 3; // this is the most amount of choices we can have 
@@ -19,10 +17,9 @@ function getComputerChoice() {
     // this function will return either rock paper scissor
     return choice;
 }
-// console.log(getComputerChoice());
 
 // delcare a function that will ask player to make a selection
-function playerChoice() {
+function getplayerChoice() {
     // ask for user input 
     let choice = prompt("Please type in a choice: rock, paper, or scissor?");
        // correct the input to all lower case no spaces
@@ -30,5 +27,116 @@ function playerChoice() {
  
     return choice;
 }
-// console.log(playerChoice());
+// declare a function playRound that takes a players choice and computer choice and outputs a winner
+// inputs: player and computer selections
+function playRound (playerSelection,computerSelection){
+    // declare a variable to store the winner and loser
+    let winner;
+    let loser;
+    // make a switch case depending on wha tthe player selection is
+    // make a swithc case for rock
+    // make a switch case for choosing paper
+    // make a switch case for choosing scissor
+    switch (playerSelection){
+        case "rock":
+            switch(computerSelection){
+                case "rock":
+                    winner = "Tie";
+                    loser = "Tie";
+                    break;
+                case "paper":
+                    winner ="Computer";
+                    loser ="Player";
+                    break;
+                case "scissor":
+                    winner = "Player";
+                    loser = "Computer";
+            }
+            break;
+        case "paper":
+            switch(computerSelection){
+                case "rock":
+                    winner = "Player";
+                    loser = "Computer";
+                    break;
+                case "paper":
+                    winner ="Tie";
+                    loser ="Tie";
+                    break;
+                case "scissor":
+                    winner = "Computer";
+                    loser = "Player";
+            }
+            break;
+        case "scissor":
+            switch(computerSelection){
+                case "rock":
+                    winner = "Computer";
+                    loser = "Player";
+                    break;
+                case "paper":
+                    winner ="Player";
+                    loser ="Computer";
+                    break;
+                case "scissor":
+                    winner = "Tie";
+                    loser = "Tie";
+            }
+        }
+    //create conditional to declare winner 
+    // output: string with winner
+    // if (winner === "Tie" && loser === "Tie") {
+    //     return console.log("We have a tie!")
+    // } else {
+    //     return console.log("Winner is: " + winner + " Loser is: " +loser);
+    // }
+
+    //refactor the return to return the winner
+    return winner
+    
+}
+
+
+// declare a variable that stored the computer selection
+// let computerSelection = getComputerChoice();
+// console.log(computerSelection);
+// declare a variable that stores players choice;
+// let playerSelection= getplayerChoice();
+// console.log(playerSelection);
+// play a round
+// playRound(playerSelection,computerSelection);
+
+//declare a function game that will play round up the 5 rounds and keep score
+function game (){
+    // declare variables to store scores and ties
+    let playerScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
+
+
+    // create a for loop to iterate through the game 5 times 
+    for (let i = 0; i <5; i++){ // increment scores based on winner or ties
+        // get selection for computer and player
+        let playerSelection = getplayerChoice();
+        console.log("You chose: " + playerSelection)
+        let computerSelection = getComputerChoice();
+        console.log("Computer chose: " + computerSelection)
+        const winner = playRound(playerSelection, computerSelection);
+        console.log("The winner is: " + winner);
+        if (winner === "Tie") {
+            tieScore++;
+        } else if (winner === "Player") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    }
+    // console log results
+    console.log("Results: Player score: " + playerScore + " Computer score: " + computerScore + " Ties: " + tieScore);
+}
+game();
+
+
+
+
 
