48757b0f5ea8f098b4340ab17adee1b16b47e580
Nicholas <lynick14@gmail.com>
2022-12-05 14:13:49 -0500

    create function for plater choice prompt

--- scripts.js
+++ scripts.js
@@ -2,21 +2,33 @@
 
 
 // declare a function that will get computer choice for rock paper scissor
-function getComputerChoice{
+function getComputerChoice() {
     let max = 3; // this is the most amount of choices we can have 
     let choice;
     // use some sort of random number generator and depending on the number it will output a specified choice
     switch ( Math.floor(Math.random()*max)){ 
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
         case 1: 
-            choice = "Paper";
+            choice = "paper";
             break;
         case 2: 
-            choice = "Scissor";
+            choice = "scissor";
     }
     // this function will return either rock paper scissor
-    return console.log(choice);
+    return choice;
 }
+// console.log(getComputerChoice());
+
+// delcare a function that will ask player to make a selection
+function playerChoice() {
+    // ask for user input 
+    let choice = prompt("Please type in a choice: rock, paper, or scissor?");
+       // correct the input to all lower case no spaces
+    choice = choice.toLowerCase();
+ 
+    return choice;
+}
+// console.log(playerChoice());
 
